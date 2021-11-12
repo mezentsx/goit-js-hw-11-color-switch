09f02e4f84a69319426a1cd37bc1deee8c3277a1
mezentsx <mezentsxn@gmail.com>
2021-11-12 14:54:27 +0100

    Поправка

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const colors = [
 const refs = {
     startBtn: document.querySelector('[data-action="start"]'),
     stopBtn: document.querySelector('[data-action="stop"]'),
-    body = document.querySelector('body'),
+    body: document.querySelector('body'),
 };
 
 const min = 0;
